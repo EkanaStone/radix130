4234da29a321bb7a22ad16c203cd99813b3b4b94
Ekana Stone <e.t.jordan14@pm.me>
2023-03-29 21:09:17 -0700

    added classes and copy function

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,35 @@ public class Main
 {
     public static void main(String[] args) throws Exception
     {
-        
+        //chang numbers
+        int[] arr = {420, 96, 69, 42960, 94206, 69420, 5, 88, 65, 234};
+
+        //copy array
+        int[] carbon = Radix.carbon(arr, arr.length);
+
     }
+}
+
+class Node
+{
+
+}
+
+class Radix
+{
+    public static int[] carbon(int[] arr, int length)
+    {
+        int[] temp = new int[length];
+
+        for (int i = 0; i<length; i++)
+        {
+            temp[i] = arr[i];
+        }
+        return temp;
+    }
+}
+
+class Queue 
+{
+
 }
